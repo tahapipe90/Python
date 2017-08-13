43faaa5e35e8e8ef4288e1bf21328b7e5b4818cc
Taha Pipewala <tahapipe90@gmail.com>
2017-08-13 14:45:55 +0530

    Add files via upload

---
+++ raw_input.py
@@ -0,0 +1,5 @@
+#taking input using raw_input function
+
+num=raw_input("enter a number\n")  #input is stored in num as string cause raw_input takes input in string
+
+print 'double of the number is %d' % (int(num)*2) #here typecasting is done string is converted into integer type
